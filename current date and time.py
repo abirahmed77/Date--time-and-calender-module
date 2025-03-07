b0a71fc5d204c54e216e3ec471f8fa007ed5a142
Abir <abirgpx@gmail.com>
2025-03-07 15:09:52 +0600

    first commit

---
+++ current date and time.py
@@ -0,0 +1,16 @@
+import date, datetime
+
+today = date.today()
+
+print(f"Today's date is : {today}")
+print()
+
+now = datetime.now()
+
+print(f"The current time at Grenwitch : {now}")
+
+print()
+
+print(f"Today's day is : {today.day}")
+print(f"Current month is : {today.mouth}")
+print(f"Current year is : {today.year}")
