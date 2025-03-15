b486ac1bf4b354659a3f7a0c332f153ff33be8e9
Abir <abirgpx@gmail.com>
2025-03-15 15:05:35 +0600

    hi

--- current date and time.py
+++ current date and time.py
@@ -1,4 +1,4 @@
-form datetime import date, datetime
+from datetime import date, datetime
 
 today = date.today()
 
@@ -12,5 +12,5 @@ print(f"The current time at Grenwitch : {now}")
 print()
 
 print(f"Today's day is : {today.day}")
-print(f"Current month is : {today.mouth}")
+print(f"Current month is : {today.month}")
 print(f"Current year is : {today.year}")
